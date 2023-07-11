e828275b4c4d1f0ecbbda398ad6ccccc935167f9
AkagiYui <akagiyui@yeah.net>
2023-07-11 09:42:25 +0800

    chore: add type hints

--- env.d.ts
+++ env.d.ts
@@ -18,3 +18,9 @@ interface ImportMeta {
 
 /** 项目版本号 */
 declare const APP_VERSION: string;
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const vueComponent: DefineComponent<{}, {}, any>
+  export default vueComponent
+}
