74ee3aff018e9efde92d597a53e1fff493aff60a
AkagiYui <akagiyui@yeah.net>
2024-03-14 23:26:27 +0800

    feat: add umami analysis

--- src/main.ts
+++ src/main.ts
@@ -24,5 +24,25 @@ window.addEventListener("unhandledrejection", (event) => {
   console.error("Unhandled Rejection: " + event.reason)
 })
 
-app.mount('#app')
+function injectUmami() {
+  // 读取环境变量
+  const url = import.meta.env.VITE_UMAMI_SCRIPT_URL
+  const id = import.meta.env.VITE_UMAMI_WEBSITE_ID
+
+  // 创建一个新的script元素
+  const script = document.createElement("script")
+  script.async = true
+  script.src = url
+  script.setAttribute("data-website-id", id)
+  // 将script标签添加到document的head中
+  document.head.appendChild(script)
+}
+
+// 仅在生产环境中注入umami统计脚本
+if (import.meta.env.PROD) {
+  injectUmami()
+} else {
+  console.debug("Development mode")
+}
+
 app.mount("#app")
