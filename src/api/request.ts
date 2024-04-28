4a692ebfc28a773ce967f583595223194c053884
AkagiYui <akagiyui@yeah.net>
2024-04-28 16:14:28 +0800

    fix: only reject when status code not in 2xx

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,7 @@
-import ServiceStatus from "@/types/ServiceStatus"
 import { useUserInfo } from "@/stores/user-info"
 import axios from "axios"
+import { NIcon } from "naive-ui"
+import { BugReportOutlined } from "@vicons/material"
 
 const isDev = import.meta.env.DEV
 const request = axios.create({
@@ -30,56 +31,54 @@ request.interceptors.request.use(
   },
 )
 
-request.interceptors.response.use(
-  (response) => {
-    // 2xx 范围内的状态码都会触发该函数。
+function responseSuccess(response: any) {
+  // 2xx 范围内的状态码都会触发该函数。
+  if (isDev) {
+    console.log("response", response)
+  }
+  window.$loadingbar.finish()
+  const contentType = response.headers["content-type"]
+  if (contentType && contentType.indexOf("application/json") !== -1) {
+    response.data = response.data["data"]
+  }
+  return response
+}
+
+// 超出 2xx 范围的状态码都会触发该函数。
+function responseFailed(error: any) {
+  window.$loadingbar.error()
+  if (error.code === "ECONNABORTED") {
+    return Promise.reject(Error("请求超时"))
+  }
+  const statusCode = error.response?.status
+  if (statusCode === 401) {
+    // 未认证，删除token并跳转到登录页
+    // ! todo 如果用户在编辑内容时，会丢失编辑内容，需要提示用户
+    const { deleteToken } = useUserInfo()
+    deleteToken()
+  } else if (statusCode === 403) {
+    // 无权限
+    window.$message.error("无权限")
+  } else if (statusCode === 400) {
+    // 参数错误、业务异常
     if (isDev) {
-      console.log("response", response)
-    }
-    const contentType = response.headers["content-type"]
-    if (contentType && contentType.indexOf("application/json") !== -1) {
-      const code: number = response.data["code"]
-      if (code !== ServiceStatus.SUCCESS) {
-        // 发生业务错误
-        window.$loadingbar.error()
-        console.error("response error", response.data)
-        switch (code) {
-          case ServiceStatus.EXIST:
-            throw new ExistException()
-          default:
-            throw new Error(response.data["message"])
-        }
-      } else {
-        window.$loadingbar.finish()
+      const contentType = error.response.headers["content-type"]
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        const message = error.response.data["msg"]
+        window.$message.warning(message, {
+          icon: () => h(NIcon, null, { default: () => h(BugReportOutlined) }),
+          duration: 10000,
+          keepAliveOnHover: true,
+        })
       }
-      response.data = response.data["data"]
-    } else {
-      window.$loadingbar.finish()
     }
-    return response
-  },
-  (error) => {
-    // 超出 2xx 范围的状态码都会触发该函数。
-    window.$loadingbar.error()
+  } else {
+    console.log("response error", error)
+  }
 
-    if (error.code === "ECONNABORTED") {
-      throw new Error("请求超时")
-    }
-    const statusCode = error.response?.status
-    if (statusCode === 401) {
-      // 未认证，删除token并跳转到登录页
-      // ! todo 如果用户在编辑内容时，会丢失编辑内容，需要提示用户
-      const { deleteToken } = useUserInfo()
-      deleteToken()
-    } else if (statusCode === 403) {
-      // 无权限
-      window.$message.error("无权限")
-    } else {
-      console.log("response error", error)
-    }
+  return Promise.reject(error)
+}
 
-    return Promise.reject(error)
-  },
-)
+request.interceptors.response.use(responseSuccess, responseFailed)
 
 export default request
