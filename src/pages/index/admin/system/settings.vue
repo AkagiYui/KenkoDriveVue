f0083f9e1575d4deba886b012a298612e9a2a41f
AkagiYui <akagiyui@yeah.net>
2024-06-12 00:42:52 +0800

    chore: remove useless n-flex

--- src/pages/index/admin/system/settings.vue
+++ src/pages/index/admin/system/settings.vue
@@ -84,115 +84,113 @@ const updateSettingsDebounced = useDebounce(() => {
 
 <template>
   <div style="padding: 20px; width: 500px; margin: 0 auto">
-    <n-flex vertical>
-      <n-form label-placement="left" label-width="auto" require-mark-placement="right-hanging" :show-feedback="true">
-        <n-h3>用户注册</n-h3>
-        <n-form-item label="开放注册">
-          <n-switch v-model:value="settings.registerEnabled" :disabled="isLoading" />
-        </n-form-item>
-        <n-form-item label="注册需要邮箱验证">
-          <n-switch :disabled="true" :value="true" />
-        </n-form-item>
-        <n-divider />
-        <n-h3>SMTP服务</n-h3>
-        <n-form-item label="SMTP服务器">
-          <n-input
-            v-model:value="settings.smtpHost as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'off' }"
-          />
-        </n-form-item>
-        <n-form-item label="SMTP端口">
-          <n-input-number
-            v-model:value="settings.smtpPort as number"
-            :allow-input="(value: string) => !value || /^\d+$/.test(value)"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'off' }"
-            :max="65535"
-            :min="0"
-            style="width: 100%"
-          />
-        </n-form-item>
-        <n-form-item label="使用 SSL">
-          <n-switch v-model:value="settings.smtpSsl" :disabled="isLoading" />
-        </n-form-item>
-        <n-form-item label="SMTP用户名">
-          <n-input
-            v-model:value="settings.smtpUsername as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'email' }"
-          />
-        </n-form-item>
-        <n-form-item label="SMTP密码">
-          <n-input
-            v-model:value="settings.smtpPassword as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'new-password' }"
-            show-password-on="click"
-            type="password"
-          />
-        </n-form-item>
-        <n-form-item label="发件人邮箱">
-          <n-input
-            v-model:value="settings.mailFrom as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'email' }"
-          />
-        </n-form-item>
+    <n-form label-placement="left" label-width="auto" require-mark-placement="right-hanging" :show-feedback="true">
+      <n-h3>用户注册</n-h3>
+      <n-form-item label="开放注册">
+        <n-switch v-model:value="settings.registerEnabled" :disabled="isLoading" />
+      </n-form-item>
+      <n-form-item label="注册需要邮箱验证">
+        <n-switch :disabled="true" :value="true" />
+      </n-form-item>
+      <n-divider />
+      <n-h3>SMTP服务</n-h3>
+      <n-form-item label="SMTP服务器">
+        <n-input
+          v-model:value="settings.smtpHost as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'off' }"
+        />
+      </n-form-item>
+      <n-form-item label="SMTP端口">
+        <n-input-number
+          v-model:value="settings.smtpPort as number"
+          :allow-input="(value: string) => !value || /^\d+$/.test(value)"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'off' }"
+          :max="65535"
+          :min="0"
+          style="width: 100%"
+        />
+      </n-form-item>
+      <n-form-item label="使用 SSL">
+        <n-switch v-model:value="settings.smtpSsl" :disabled="isLoading" />
+      </n-form-item>
+      <n-form-item label="SMTP用户名">
+        <n-input
+          v-model:value="settings.smtpUsername as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'email' }"
+        />
+      </n-form-item>
+      <n-form-item label="SMTP密码">
+        <n-input
+          v-model:value="settings.smtpPassword as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'new-password' }"
+          show-password-on="click"
+          type="password"
+        />
+      </n-form-item>
+      <n-form-item label="发件人邮箱">
+        <n-input
+          v-model:value="settings.mailFrom as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'email' }"
+        />
+      </n-form-item>
 
-        <n-form-item v-if="false" label="SMTP加密方式">
-          <n-select
-            v-if="false"
-            :disabled="true"
-            :options="[
-              {
-                label: 'Everybody\'s Got Something to Hide Except Me and My Monkey',
-                value: 'song0',
-                disabled: true,
-              },
-              {
-                label: 'Drive My Car',
-                value: 'song1',
-              },
-            ]"
-          />
-        </n-form-item>
-        <n-divider />
-        <n-h3>阿里云短信服务</n-h3>
-        <n-form-item label="AccessKeyId">
-          <n-input
-            v-model:value="settings.aliyunSmsAccessKeyId as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'off' }"
-            show-password-on="click"
-            type="password"
-          />
-        </n-form-item>
-        <n-form-item label="AccessKeySecret">
-          <n-input
-            v-model:value="settings.aliyunSmsAccessKeySecret as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'off' }"
-            show-password-on="click"
-            type="password"
-          />
-        </n-form-item>
-        <n-form-item label="短信签名">
-          <n-input
-            v-model:value="settings.aliyunSmsSignName as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'off' }"
-          />
-        </n-form-item>
-        <n-form-item label="短信模板">
-          <n-input
-            v-model:value="settings.aliyunSmsTemplateCode as string"
-            :disabled="isLoading"
-            :input-props="{ autocomplete: 'off' }"
-          />
-        </n-form-item>
-      </n-form>
-    </n-flex>
+      <n-form-item v-if="false" label="SMTP加密方式">
+        <n-select
+          v-if="false"
+          :disabled="true"
+          :options="[
+            {
+              label: 'Everybody\'s Got Something to Hide Except Me and My Monkey',
+              value: 'song0',
+              disabled: true,
+            },
+            {
+              label: 'Drive My Car',
+              value: 'song1',
+            },
+          ]"
+        />
+      </n-form-item>
+      <n-divider />
+      <n-h3>阿里云短信服务</n-h3>
+      <n-form-item label="AccessKeyId">
+        <n-input
+          v-model:value="settings.aliyunSmsAccessKeyId as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'off' }"
+          show-password-on="click"
+          type="password"
+        />
+      </n-form-item>
+      <n-form-item label="AccessKeySecret">
+        <n-input
+          v-model:value="settings.aliyunSmsAccessKeySecret as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'off' }"
+          show-password-on="click"
+          type="password"
+        />
+      </n-form-item>
+      <n-form-item label="短信签名">
+        <n-input
+          v-model:value="settings.aliyunSmsSignName as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'off' }"
+        />
+      </n-form-item>
+      <n-form-item label="短信模板">
+        <n-input
+          v-model:value="settings.aliyunSmsTemplateCode as string"
+          :disabled="isLoading"
+          :input-props="{ autocomplete: 'off' }"
+        />
+      </n-form-item>
+    </n-form>
   </div>
 </template>
 
