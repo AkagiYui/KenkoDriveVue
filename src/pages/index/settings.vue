e5c59f1ec84d1260d44a2ed897e769fb88a9fcba
AkagiYui <akagiyui@yeah.net>
2024-08-23 16:39:00 +0800

    refactor: user settings view

--- src/pages/index/settings.vue
+++ src/pages/index/settings.vue
@@ -15,20 +15,12 @@ import { useAppConfig } from "@/stores/app-config"
 import { useUserInfo } from "@/stores/user-info"
 import { uploadUserAvatar } from "@/api"
 import { Permission } from "@/types"
+import { useDebounce } from "@/hooks"
 
 const { isDarkMode, isDebugMode } = storeToRefs(useAppConfig())
 const { toggleDarkMode, reset: resetConfig } = useAppConfig()
-const { avatarUrl, username, nickname, email } = storeToRefs(useUserInfo())
+const { avatarUrl, nickname: nicknameInStore, email, phone } = storeToRefs(useUserInfo())
 const { renewAvatar, hasPermission } = useUserInfo()
-const personalInfo = ref({
-  nickname: "",
-  email: "",
-})
-
-onBeforeMount(() => {
-  personalInfo.value.nickname = nickname.value
-  personalInfo.value.email = email.value
-})
 
 const reset = () => {
   resetConfig()
@@ -49,123 +41,103 @@ const uploadAvatar = async (fileList: UploadFileInfo[]) => {
   fileInfo.status = "finished"
   avatarFileList.value = []
 }
+
+const nickname = ref(nicknameInStore.value)
+const updateNickname = useDebounce(() => {
+  nicknameInStore.value = nickname.value
+  window.$message.success("昵称已更新")
+}, 500)
+watch(nickname, updateNickname)
 </script>
 
 <template>
-  <div style="padding: 24px">
-    <n-space vertical>
-      <n-card title="个人信息">
+  <div style="padding: 24px; margin: 0 auto; width: 500px">
+    <n-form label-placement="left" label-width="auto" require-mark-placement="right-hanging" :show-feedback="true">
+      <n-h3>个人信息</n-h3>
+      <n-form-item label="头像">
+        <n-upload
+          v-model:file-list="avatarFileList"
+          accept="image/*"
+          :max="1"
+          :show-file-list="false"
+          :show-cancel-button="false"
+          :default-upload="false"
+          @update:file-list="uploadAvatar"
+        >
+          <n-upload-dragger style="margin: 0; padding: 0; height: 100px; width: 100px">
+            <n-image
+              object-fit="fill"
+              preview-disabled
+              width="96"
+              height="96"
+              :src="avatarUrl"
+              style="margin: 0; padding: 1px; height: 100px; width: 100px"
+            />
+          </n-upload-dragger>
+        </n-upload>
+      </n-form-item>
+      <n-form-item label="昵称">
+        <n-input v-model:value="nickname" :input-props="{ autocomplete: 'off' }" />
+      </n-form-item>
+      <n-form-item label="邮箱">
         <n-space>
-          <n-upload
-            v-model:file-list="avatarFileList"
-            accept="image/*"
-            :max="1"
-            :show-file-list="false"
-            :show-cancel-button="false"
-            :default-upload="false"
-            @update:file-list="uploadAvatar"
-          >
-            <n-space vertical>
-              <n-upload-dragger style="margin: 0; padding: 0; height: 100px; width: 100px">
-                <n-image
-                  object-fit="fill"
-                  preview-disabled
-                  width="96"
-                  height="96"
-                  :src="avatarUrl"
-                  style="margin: 0; padding: 1px; height: 100px; width: 100px"
-                />
-              </n-upload-dragger>
-              <n-space justify="center">
-                <n-button dashed size="small"> 上传头像</n-button>
-              </n-space>
-            </n-space>
-          </n-upload>
-
-          <n-space>
-            <n-space vertical>
-              <n-input-group>
-                <n-input-group-label class="info-label"> 用户名 </n-input-group-label>
-                <n-tooltip trigger="hover" placement="top">
-                  <template #trigger>
-                    <n-input :value="username" disabled />
-                  </template>
-                  用户名不可修改
-                </n-tooltip>
-              </n-input-group>
-              <n-input-group>
-                <n-input-group-label class="info-label"> 昵称 </n-input-group-label>
-                <n-input v-model:value="personalInfo.nickname" />
-              </n-input-group>
-              <n-input-group>
-                <n-input-group-label class="info-label"> 邮箱 </n-input-group-label>
-                <n-input v-model:value="personalInfo.email" />
-              </n-input-group>
-              <n-input-group>
-                <n-input-group-label class="info-label"> 手机号 </n-input-group-label>
-                <n-input />
-              </n-input-group>
-            </n-space>
-            <n-button style="height: 100%"> 保存</n-button>
-          </n-space>
+          <n-input :value="email" :input-props="{ autocomplete: 'off' }" />
+          <n-button type="warning" secondary> 更换邮箱 </n-button>
         </n-space>
-      </n-card>
-
-      <n-card title="外观">
-        <n-space vertical>
-          <n-space style="display: flex">
-            <n-button-group>
-              <span style="align-self: center; margin-right: 10px">主题</span>
-              <n-button :type="!isDarkMode ? 'primary' : 'default'" @click="toggleDarkMode">
-                <template #icon>
-                  <n-icon>
-                    <WeatherSunny16Regular />
-                  </n-icon>
-                </template>
-                亮色
-              </n-button>
-              <n-button :type="isDarkMode ? 'primary' : 'default'" @click="toggleDarkMode">
-                <template #icon>
-                  <n-icon>
-                    <WeatherMoon16Regular />
-                  </n-icon>
-                </template>
-                暗色
-              </n-button>
-            </n-button-group>
-          </n-space>
-          <n-space
-            v-if="hasPermission(Permission.FRONTEND_ENABLE_DEBUG_MODE)"
-            style="display: flex; align-items: center"
-          >
-            <n-text>调试模式</n-text>
-            <n-switch v-model:value="isDebugMode" :round="false" />
-          </n-space>
+      </n-form-item>
+      <n-form-item label="手机号">
+        <n-space>
+          <n-input :value="phone" :input-props="{ autocomplete: 'off' }" />
+          <n-button type="warning" secondary>更换手机号 </n-button>
         </n-space>
-      </n-card>
+      </n-form-item>
+      <n-form-item label="密码">
+        <n-button type="warning" secondary>修改密码 </n-button>
+      </n-form-item>
 
-      <n-card title="恢复">
-        <n-space vertical>
-          <n-popconfirm
-            :positive-button-props="{
-              type: 'error',
-            }"
-            @positive-click="reset"
-          >
-            <template #trigger>
-              <n-button type="error"> 重置面板缓存</n-button>
+      <n-divider />
+      <n-h3>外观</n-h3>
+      <n-form-item label="主题">
+        <n-button-group>
+          <n-button :type="!isDarkMode ? 'primary' : 'default'" secondary @click="toggleDarkMode">
+            <template #icon>
+              <n-icon>
+                <WeatherSunny16Regular />
+              </n-icon>
             </template>
-            是否要重置面板缓存？<br />该操作不会清除用户数据。
-          </n-popconfirm>
-        </n-space>
-      </n-card>
-    </n-space>
+            亮色
+          </n-button>
+          <n-button :type="isDarkMode ? 'primary' : 'default'" secondary @click="toggleDarkMode">
+            <template #icon>
+              <n-icon>
+                <WeatherMoon16Regular />
+              </n-icon>
+            </template>
+            暗色
+          </n-button>
+        </n-button-group>
+      </n-form-item>
+
+      <n-divider />
+      <n-h3>调试</n-h3>
+      <n-form-item v-if="hasPermission(Permission.FRONTEND_ENABLE_DEBUG_MODE)" label="测试组件">
+        <n-switch v-model:value="isDebugMode" :round="true" />
+      </n-form-item>
+      <n-form-item label="重置">
+        <n-popconfirm
+          :positive-button-props="{
+            type: 'error',
+          }"
+          @positive-click="reset"
+        >
+          <template #trigger>
+            <n-button type="error"> 重置面板缓存</n-button>
+          </template>
+          是否要重置面板缓存？<br />该操作不会清除用户数据。
+        </n-popconfirm>
+      </n-form-item>
+    </n-form>
   </div>
 </template>
 
-<style scoped>
-.info-label {
-  width: 70px;
-  text-align: center;
-}
-</style>
+<style scoped></style>
